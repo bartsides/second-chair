25c98fd95115998ef736a55e7c87cdf1f099d897
Bart Sides <bartsides@gmail.com>
2024-02-06 18:11:25 -0500

    Added Selection and Trial modes. Adjusted where jurors are ordered depending on the destination list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,34 +4,49 @@ import {
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { faker } from '@faker-js/faker';
-import { Juror } from './models/juror';
-import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker';
 import { JurorCardComponent } from './juror-card/juror-card.component';
 import { JurorEditComponent } from './juror-edit/juror-edit.component';
+import { Juror } from './models/juror';
+
+enum Mode {
+  Selection,
+  Trial,
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DragDropModule, JurorCardComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    DragDropModule,
+    JurorCardComponent,
+    MatIconModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'jury-select';
+  modes = ['Selection', 'Trial'];
+  mode = this.modes[0];
   pool: Juror[] = [];
   selected: Juror[] = [];
-  rejected: Juror[] = [];
+  notSelected: Juror[] = [];
+  selectedJuror: Juror;
 
   constructor(public dialog: MatDialog) {
     this.fakeData();
   }
 
   fakeData() {
-    for (var i = 0; i < 20; i++) {
+    for (var i = 0; i < 4; i++) {
       this.pool.push(this.generateJuror());
     }
     for (var i = 0; i < 4; i++) {
@@ -46,30 +61,41 @@ export class AppComponent {
     };
   }
 
+  changeMode(mode: string) {
+    this.mode = mode;
+  }
+
   jurorClicked(juror: Juror) {
-    console.log('juror clicked', juror);
+    this.selectedJuror = juror;
     const dialogRef = this.dialog.open(JurorEditComponent, { data: { juror } });
     dialogRef.afterClosed().subscribe((res) => {
       console.log('Dialog closed', res);
+      if (res !== undefined) this.selectedJuror = res;
+      console.log(this.selectedJuror, res);
     });
   }
 
   drop(event: CdkDragDrop<Juror[]>) {
+    console.log(event);
+
     if (event.previousContainer === event.container) {
       // Reorder items within the same list
-      moveItemInArray(
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
+      if (event.container.id == 'selected-jurors') return;
+      // Move to front
+      moveItemInArray(event.container.data, event.previousIndex, 0);
     } else {
-      // TODO: Move juror to end of list
       // Move items between lists
+      var index = event.container.data.length;
+      if (event.container.id == 'juror-pool') {
+        index = 0;
+      } else if (event.container.id == 'selected-jurors') {
+        // TODO: Set juror number
+      }
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex
+        index
       );
     }
   }
